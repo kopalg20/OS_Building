78cc3d2554697e52f5bd4ff794d3d1f517e5a985
kopalg20 <kopalgarg2005@gmail.com>
2026-01-09 00:12:57 +0530

    Bootsector returning a text HELLO

--- bootsector_load.asm
+++ bootsector_load.asm
@@ -1,4 +1,15 @@
-loop:
-    jmp loop
-times 510-($-$$) db 0
+mov ah, 0x0e
+mov al, 'H'
+int 0x10
+mov al, 'E'
+int 0x10
+mov al, 'L'
+int 0x10
+int 0x10
+mov al, '0'
+int 0x10
+
+jmp $
+
+times 510 - ($-$$) db 0
 dw 0xaa55
